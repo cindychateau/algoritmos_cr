23b8dcd0d751d6047b9b75c70a411c5ebc4dcd2d
Cynthia Castillo <ca.castillo@gmail.com>
2023-10-09 20:51:06 -0600

    LDE completo

--- listasDoblementeEncadenadas/LDE.js
+++ listasDoblementeEncadenadas/LDE.js
@@ -6,16 +6,53 @@ module.exports = class LDE {
         this.head = null;
     }
 
-    insertaNodo(nuevoNodo) {
 
+    insertaNodo(nuevoNodo) {
+        if(!this.head) { //this.head == null
+            this.head = nuevoNodo;
+        } else {
+            let nodoActual = this.head;
+            while(nodoActual.next) { //el siguiente sea diferente a nulo
+                nodoActual = nodoActual.next;
+            }
+            //Del último elemento de mi lista, el siguiente es el nuevoNodo
+            nodoActual.next = nuevoNodo;
+            nuevoNodo.before = nodoActual;
+        }
     }
 
     imprimeLista() {
-
+        let aux = this.head;
+        while(aux) { //aux != null
+            console.log(aux.valor);
+            aux = aux.next;
+        }
     }
-
+    /*
+    valorABorrar = Pedro
+    nodoActual = Elena
+    nodoActual = Pedro
+    nodoAnterior = Elena
+    nodoSiguiente = Pablo
+    Elena.next = Pablo
+    Pablo.before = Elena
+    Pedro.before = null
+    Pedro.next = null
+     */
     borrarNodo(valorABorrar) {
+        let nodoActual = this.head;
+        while(nodoActual) { //nodoActual sea diferente a nulo
+            if(nodoActual.valor === valorABorrar) {
+                let nodoAnterior = nodoActual.before;
+                let nodoSiguiente = nodoActual.next;
+                nodoAnterior.next = nodoSiguiente;
+                nodoSiguiente.before = nodoAnterior;
 
+                nodoActual.before = null;
+                nodoActual.next = null;
+            }
+            nodoActual = nodoActual.next;
+        }
     }
 
 }
